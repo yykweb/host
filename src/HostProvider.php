7ac3f96921ed79834c5e267f8e1aff104e282a9c
yykweb <lizhen@youyike.cn>
2017-01-07 14:34:48 +0800

    更新

--- src/HostProvider.php
+++ src/HostProvider.php
@@ -4,5 +4,15 @@ use houdunwang\framework\build\Provider;
 
 class HostProvider extends Provider
 {
-    
+    //延迟加载
+    public $defer = true;
+
+    public function boot() {
+    }
+
+    public function register() {
+        $this->app->single( 'Host', function (  ) {
+            return new Host(  );
+        } );
+    }
 }
